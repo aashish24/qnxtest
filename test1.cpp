2b06df9d0afc8af1b6d263359ac99d1b43907c0e
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-02-16 10:47:17 -0500

    Removed unused files

--- test1.cpp
+++ test1.cpp
@@ -6,15 +6,15 @@
 #include <gf/gf.h>
 #include <stdio.h>
 int main(int argc, char *argv[]) {
-	gf_dev_t    gfdev;
-	gf_dev_info_t       info;
-	  int res =0;//= gf_dev_attach(&gfdev, NULL, &info);
-	  if (res != GF_ERR_OK)
-	  {
-	     printf("gf_dev_attach() %d\n", res);
-	     return -1;
-	  }
-	  printf("gf_dev_attach() %d\n", res);
-	std::cout << "Welcome to the QNX Momentics IDE" << std::endl;
-	return EXIT_SUCCESS;
+  gf_dev_t    gfdev;
+  gf_dev_info_t       info;
+  int res = gf_dev_attach(&gfdev, NULL, &info);
+  if (res != GF_ERR_OK)
+  {
+     printf("gf_dev_attach() failed %d\n", res);
+     return -1;
+  }
+  printf("gf_dev_attach() passed %d\n", res);
+  std::cout << "Welcome to the QNX Momentics IDE" << std::endl;
+  return EXIT_SUCCESS;
 }
