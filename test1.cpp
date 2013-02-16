44319e1e64f5517f9ccd17cf6fa527298158982c
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-02-16 10:25:07 -0500

    Initial import

---
+++ test1.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <iostream>
+#include <GLES/gl.h>
+#include <GLES/egl.h>
+
+#include <gf/gf.h>
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+	gf_dev_t    gfdev;
+	gf_dev_info_t       info;
+	  int res =0;//= gf_dev_attach(&gfdev, NULL, &info);
+	  if (res != GF_ERR_OK)
+	  {
+	     printf("gf_dev_attach() %d\n", res);
+	     return -1;
+	  }
+	  printf("gf_dev_attach() %d\n", res);
+	std::cout << "Welcome to the QNX Momentics IDE" << std::endl;
+	return EXIT_SUCCESS;
+}
